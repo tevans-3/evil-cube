a68c8ae305f14062b4b2fae563726f20d01755e5
tevans-3 <tevans3@ualberta.ca>
2026-06-12 12:24:29 -0600

    feat: refactored into OOP

--- src/main.js
+++ src/main.js
@@ -13,6 +13,11 @@ import { LineSegments2 } from 'three/addons/lines/LineSegments2.js';
 // 3. https://cs.stanford.edu/people/karpathy/reinforcejs/
 //
 
+let canvas; 
+const pickPosition = {x: 0, y: 0}; 
+const mousePosition = {x: 0, y: 0};
+var mouseMoveDirection = ''; 
+
 const debug = true; 
 
 function _principalComponent(v) { 
@@ -31,28 +36,79 @@ function _principalComponent(v) {
     return maxAxis; 
 }
 
-class GestureStateMachine {
-    constructor() { 
+function _getCanvasRelativePosition(event) {
+  const rect = canvas.getBoundingClientRect();
+  return {
+    x: (event.clientX - rect.left) * canvas.width  / rect.width,
+    y: (event.clientY - rect.top ) * canvas.height / rect.height,
+  };
+}
+ 
+function _setPickPosition(event) {
+  const pos = _getCanvasRelativePosition(event);
+  pickPosition.x = (pos.x / canvas.width ) *  2 - 1;
+  pickPosition.y = (pos.y / canvas.height) * -2 + 1;  
+}
+ 
+function _clearPickPosition() {
+  pickPosition.x = -100000;
+  pickPosition.y = -100000;
+}
+
+
+
+class InteractionState { 
+    constructor(mousePosition, clickedOnPoint=null, 
+                rotateAroundAxis=null, clickedOnFace=null, 
+                clickedOnFacePlane=null, dragDir=null) { 
+        this.mousePosition = mousePosition; 
+        this.clickedOnPoint = clickedOnPoint; 
+        this.rotateAroundAxis = rotateAroundAxis; 
+        this.clickedOnFace = clickedOnFace;  
+        this.clickedOnFacePlane = clickedOnFacePlane; 
+        this.dragDir = dragDir; 
+    }
+}
+
+class UserInteractionStateMachine {
+    constructor(state) { 
         this.hovering = true; 
         this.clicking = false; 
         this.dragging = false; 
         this.animating = false; 
         this.stopped = false; 
-        this.mousePosition = null; 
-        this.clickedOnPoint = null; 
-        this.rotateAroundAxis = null; 
-        this.clickedOnFace = null; 
-        this.clickedOnFacePlane = null; 
-        this.dragDir = null; 
+        this.interactionState = state; 
+    }
+
+    wipeOldState() { 
+        Object.entries(this).forEach(([key, value]) => { 
+            if (value) { 
+                this.key = false; 
+            }
+        }); 
     }
 
     update(state) { 
         switch (action) { 
-            case "hovering": 
+            case "hovering":
+                this.wipeOldState(); 
                 this.hovering = true; 
+            case "clicking":
+                this.wipeOldState(); 
+                this.clicking = true; 
+            case "dragging": 
+                this.wipeOldState(); 
+                this.dragging = true; 
+            case "animating": 
+                this.wipeOldState(); 
+                this.animating = true;  
+            case "mouseup": 
+                this.wipeOldState(); 
+                this.stopped = true; 
             default:
                 return;
         }
+        this.interactionState = state; 
     }
 }
 
@@ -76,6 +132,7 @@ class PickHelper {
         const intersectedObjects = this.raycaster.intersectObjects(scene.children, true);
 
         if (intersectedObjects.length) { 
+            if (!intersectedObjects[0].face) return;
             this.pickedObject = intersectedObjects[0].object;
             // faceNormal is the vector perpendicular to the clicked-on face 
             // of the cube's axes, of the face's in-plane axes, the face normal 
@@ -121,35 +178,114 @@ class PickHelper {
     }
 }
 
-const pickPosition = {x: 0, y: 0}; 
-const mousePosition = {x: 0, y: 0}; 
-clearPickPosition();
+class ThreeScene {
+    constructor() { 
+        this.scene = new THREE.Scene(); 
+        this.camera = new THREE.PerspectiveCamera(30, 
+            window.innerWidth / window.innerHeight); 
+        this.renderer = new THREE.WebGLRenderer({ antialias: true }); 
+        this.ambientLight = new THREE.AmbientLight('white', 1); 
+        this.light = new THREE.DirectionalLight('white', 1); 
+        this.canvas = this.renderer.domElement; 
+        this.time = 1; 
+    }
 
-function getCanvasRelativePosition(event) {
-  const rect = canvas.getBoundingClientRect();
-  return {
-    x: (event.clientX - rect.left) * canvas.width  / rect.width,
-    y: (event.clientY - rect.top ) * canvas.height / rect.height,
-  };
-}
- 
-function setPickPosition(event) {
-  const pos = getCanvasRelativePosition(event);
-  pickPosition.x = (pos.x / canvas.width ) *  2 - 1;
-  pickPosition.y = (pos.y / canvas.height) * -2 + 1;  
-}
- 
-function clearPickPosition() {
-  pickPosition.x = -100000;
-  pickPosition.y = -100000;
+    init(bg, cam, camLayer) { 
+        //this.scene.position.set(3, 5, 3);
+        this.scene.background = new THREE.Color(bg);
+        this.scene.add(this.ambientLight); 
+        this.camera.position.set(cam.x, cam.y, cam.z); 
+        this.camera.lookAt(this.scene.position); 
+        this.camera.layers.enable(camLayer); 
+        // let there be light
+        this.light.position.set(1, 1, 1); 
+        this.scene.add(this.light); 
+        this.renderer.setSize(window.innerWidth, window.innerHeight); 
+        document.body.appendChild(this.renderer.domElement); 
+    }
 }
 
-var mouseMoveDirection = ''; 
+class RubiksCube { 
+    // only 3x3 supported
+    constructor(texture) { 
+        this.NUM_CUBELETS = 27;  
+        this.NUM_CUBELETS_PER_ROW = 3; 
+        this.CUBELET_SIZE = 1/3;  
+        this.FACE_COLORS = ["#FF0000",
+                            "#FF6400",
+                            "#FFFF00",
+                            "#00BB00",
+                            "#0000BB",
+                            "#FFFFFF", 
+                            "#000000"]; 
+        this.loader = new THREE.TextureLoader(); 
+        this.texture = this.loader.load(texture); 
+    }
+
+    visualize(scene) {
+        //TODO handle non 3x3 cubes
+        var cube = new THREE.Group();
+
+        for (let i = 0; i < this.NUM_CUBELETS_PER_ROW; i++) {
+            for (let j = 0; j < this.NUM_CUBELETS_PER_ROW; j++) {
+                for (let k = 0; k < this.NUM_CUBELETS_PER_ROW; k++) {
+
+                    var cubelet = new THREE.Mesh(
+                        new THREE.BoxGeometry(this.CUBELET_SIZE, this.CUBELET_SIZE, this.CUBELET_SIZE),
+                        [
+                            new THREE.MeshPhongMaterial({ color: this.FACE_COLORS[0], alphaTest: 0.5 }),
+                            new THREE.MeshPhongMaterial({ color: this.FACE_COLORS[1], alphaTest: 0.5 }),
+                            new THREE.MeshPhongMaterial({ color: this.FACE_COLORS[2], alphaTest: 0.5 }),
+                            new THREE.MeshPhongMaterial({ color: this.FACE_COLORS[3], alphaTest: 0.5 }),
+                            new THREE.MeshPhongMaterial({ color: this.FACE_COLORS[4], alphaTest: 0.5 }),
+                            new THREE.MeshPhongMaterial({ color: this.FACE_COLORS[5], alphaTest: 0.5 }),
+                        ]
+                    );
+
+                    cubelet.position.x += i / this.NUM_CUBELETS_PER_ROW;
+                    cubelet.position.y += j / this.NUM_CUBELETS_PER_ROW;
+                    cubelet.position.z += k / this.NUM_CUBELETS_PER_ROW;
+                    cubelet.rubikPosition = cubelet.position.clone(); 
+
+                    cubelet.name = `cubelet_${i}${j}${k}`;
+                    let edgeLord = new THREE.BoxGeometry(this.CUBELET_SIZE, this.CUBELET_SIZE, this.CUBELET_SIZE); 
+                    const edges = new THREE.EdgesGeometry(edgeLord, 1); 
+                    const positions = edges.attributes.position.array;
+                    const lineGeometry = new LineSegmentsGeometry().fromEdgesGeometry(edges);  
+                    lineGeometry.setPositions(positions); 
+                    const lineMaterial = new LineMaterial({ 
+                        color: 'black', 
+                        linewidth: 10,
+                        gapSize: 1,
+                        resolution: new THREE.Vector2(window.innerWidth, window.innerHeight)})
+                    ;
+                    const cubeEdges = new LineSegments2(lineGeometry, lineMaterial);
+                    cubeEdges.layers.set(1);
+                    cubelet.add(cubeEdges);
+                    cube.add(cubelet);
+                }
+            }
+        }
+        scene.add(cube); 
+    }
+
+    rotate(cube, clickedCubelet) { 
+
+    }
+}
+
+
+/*  EVENT LISTENERS  
+    WHAT IS OUR PURPOSE IN LIFE? 
+    WE LISTEN FOR EVENTS! 
+*/ 
 
 window.addEventListener('mousedown', (e) => { 
 
-setPickPosition(e);});
-window.addEventListener('mouseup', clearPickPosition);
+_setPickPosition(e);});
+
+window.addEventListener('mouseup', _clearPickPosition);
+
 window.addEventListener('mousemove', (e) => { 
     if (e.movementX > 0) {
         mouseMoveDirection = "Right"; 
@@ -163,146 +299,41 @@ window.addEventListener('mousemove', (e) => {
         mouseMoveDirection = "Up"; 
     }
 }); 
-window.addEventListener('mouseleave', clearPickPosition);
+window.addEventListener('mouseleave', _clearPickPosition);
 
 window.addEventListener('touchstart', (event) => { 
     event.preventDefault(); 
-    setPickPosition(event.touches[0]); 
+    _setPickPosition(event.touches[0]); 
 }, {passive: false}); 
 window.addEventListener('touchmove', (event) => { 
-    setPickPosition(event.touches[0]); 
+    _setPickPosition(event.touches[0]); 
 }); 
-window.addEventListener('touchend', clearPickPosition); 
+window.addEventListener('touchend', _clearPickPosition); 
 
-const NUM_CUBELETS = 27;
+/*   DRIVER CODE   */
 
-function enableCameraControl() { 
-    orbitControl.noRotate = false; 
-}
+let rubiks = new ThreeScene(); 
+let cameraPosition = new THREE.Vector3(3,5,3); 
+rubiks.init('Black', cameraPosition, 1); 
+canvas = rubiks.canvas; 
 
-function disableCameraControl() { 
-    orbitControl.noRotate = true; 
-}
+let cube = new RubiksCube();
+cube.visualize(rubiks.scene); 
 
-//initializing scene and camera 
-const scene = new THREE.Scene();
-scene.background = new THREE.Color('black');
 
-//FOV, aspect ratio are the PerspectiveCamera attributes
-const camera = new THREE.PerspectiveCamera(30,
-    window.innerWidth / window.innerHeight);
+_clearPickPosition();
 
-camera.position.set(3, 5, 3);
-camera.lookAt(scene.position);
-camera.layers.enable(1); 
+const pickHelper = new PickHelper();
 
-//initializing renderer 
-const renderer = new THREE.WebGLRenderer({ antialias: true });
-var ambientLight = new THREE.AmbientLight('white', 1);
-scene.add(ambientLight);
-
-//let there be light
-var light = new THREE.DirectionalLight('white', 1);
-light.position.set(1, 1, 1);
-scene.add(light);
-
-renderer.setSize(window.innerWidth + 10, window.innerHeight + 10);
-document.body.appendChild(renderer.domElement);
-
-const canvas = renderer.domElement;
-document.body.appendChild(canvas);
-var time = 1;
-
-// 
-function rotate(cube, clickedCubelet) { 
-
+function animate() {
+    rubiks.time *= 0.001;
+    pickHelper.pick(pickPosition, rubiks.scene, rubiks.camera, rubiks.time); 
+    rubiks.renderer.render(rubiks.scene, rubiks.camera);
 }
 
 if (debug) { 
     // X == red, Y == green, Z == blue
-    const axesHelper = new THREE.AxesHelper(5); scene.add(axesHelper);
+    const axesHelper = new THREE.AxesHelper(5); rubiks.scene.add(axesHelper);
 }
 
-const NUM_CUBELETS_PER_ROW = 3;
-const CUBELET_SIZE = 1 / 3;
-const FACE_COLORS = ["#FF0000",
-    "#FF6400",
-    "#FFFF00",
-    "#00BB00",
-    "#0000BB",
-    "#FFFFFF", 
-    "#000000"
-];
-
-
-const loader = new THREE.TextureLoader();
-const texture = loader.load('resources/images/frame.png');
-
-const cubeletIds = {};
-
-function initRubiksCube(scene) {
-
-    var ids = Array.from({ length: 6 }, (_, index) => index);
-    var cube = new THREE.Group();
-    let id = 1;
-    for (let i = 0; i < NUM_CUBELETS_PER_ROW; i++) {
-        for (let j = 0; j < NUM_CUBELETS_PER_ROW; j++) {
-            for (let k = 0; k < NUM_CUBELETS_PER_ROW; k++) {
-
-                var cubelet = new THREE.Mesh(
-                    new THREE.BoxGeometry(CUBELET_SIZE, CUBELET_SIZE, CUBELET_SIZE),
-                    [
-                        new THREE.MeshPhongMaterial({ color: FACE_COLORS[0], alphaTest: 0.5 }),
-                        new THREE.MeshPhongMaterial({ color: FACE_COLORS[1], alphaTest: 0.5 }),
-                        new THREE.MeshPhongMaterial({ color: FACE_COLORS[2], alphaTest: 0.5 }),
-                        new THREE.MeshPhongMaterial({ color: FACE_COLORS[3], alphaTest: 0.5 }),
-                        new THREE.MeshPhongMaterial({ color: FACE_COLORS[4], alphaTest: 0.5 }),
-                        new THREE.MeshPhongMaterial({ color: FACE_COLORS[5], alphaTest: 0.5 }),
-                    ]
-                );
-
-                cubelet.position.x += i / NUM_CUBELETS_PER_ROW;
-                cubelet.position.y += j / NUM_CUBELETS_PER_ROW;
-                cubelet.position.z += k / NUM_CUBELETS_PER_ROW;
-                cubelet.rubikPosition = cubelet.position.clone(); 
-
-                cubelet.name = `cubelet_${i}${j}${k}`;
-                scene.add(cubelet);
-                let edgeLord = new THREE.BoxGeometry(CUBELET_SIZE, CUBELET_SIZE, CUBELET_SIZE); 
-                const edges = new THREE.EdgesGeometry(edgeLord, 1); 
-                const positions = edges.attributes.position.array;
-                const lineGeometry = new LineSegmentsGeometry().fromEdgesGeometry(edges);  
-                lineGeometry.setPositions(positions); 
-                const lineMaterial = new LineMaterial({ 
-                    color: 'black', 
-                    linewidth: 10,
-                    gapSize: 1,
-                    resolution: new THREE.Vector2(window.innerWidth, window.innerHeight)})
-                ;
-                const cubeEdges = new LineSegments2(lineGeometry, lineMaterial);
-                cubeEdges.layers.set(1);
-                cubelet.add(cubeEdges);
-                cube.add(cubelet);
-
-                cubeletIds[id] = cubelet;
-                id += 1;
-            }
-        }
-    }
-    return cube;
-}
-
-function handleMove() {
-    ;
-}
-
-const cube = initRubiksCube(scene);
-scene.add(cube);
-const pickHelper = new PickHelper();
-function animate() {
-    time *= 0.001;
-    pickHelper.pick(pickPosition, scene, camera, time); 
-    renderer.render(scene, camera);
-}
-
-renderer.setAnimationLoop(animate); 
+rubiks.renderer.setAnimationLoop(animate); 
