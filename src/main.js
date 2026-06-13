7c4b83decad8e8c0b656c8f29a27636737ecf373
tevans-3 <tevans3@ualberta.ca>
2026-06-13 14:55:54 -0600

    feat: added some comments

--- src/main.js
+++ src/main.js
@@ -77,10 +77,14 @@ if (debug) {
 
 rubiks.renderer.setAnimationLoop(animate); 
 
-function gestureMoveLogic(e, touched = false) { 
+function gestureMoveLogic(e, touched = false) {
     if (!stateMachine.picked && !stateMachine.dragging) return;
+
     if (touched) _setPickPosition(e.touches[0]);
     else _setPickPosition(e); 
+
+    // need to cast a ray to intersect the clicked on face plane in order 
+    // to compute the currentDragWorld, the drag in world space
     evil.mouseMoveRaycaster.layers.set(0);
     evil.mouseMoveRaycaster.setFromCamera(evil.mousePosition, rubiks.camera);
     const intersectionPoint = new THREE.Vector3();
@@ -89,38 +93,46 @@ function gestureMoveLogic(e, touched = false) {
     if (!result) return;
     const currentDragWorld = intersectionPoint.clone().sub(state.clickedOnPoint);
 
-    if (stateMachine.dragging) {
-        if (intersectionPoint && result) {
-            state.dragDistance = currentDragWorld.dot(state.dragDir)
-            let angle = _dragAngle(state.dragDistance);
-            const q = new THREE.Quaternion().setFromAxisAngle(state.rotateAroundAxis, angle);
-            pivot.quaternion.copy(q);
-            pivot.updateMatrixWorld(true);
-        }
-    }
-    else if (stateMachine.picked) {
+    // this only executes once per gesture (well, it should)
+    if (stateMachine.picked) {
+
+        // these magic numbers correspond to a fraction of cube size
+        // basically we want to only transition from PICKED -> DRAGGING
+        // when the gesture has dragged a reasonable distance, which
+        // prevents arbitrarily small drag distances from triggering a state 
+        // change; 1/3 should be replaced with an exported global in ./shared.js 
         if (currentDragWorld.length() < 1 / 3 / 3) {
             _clearPickPosition(e);
             return;
         }
+
+        // if our ray intersected the clicked on face plane 
         if (result) {
+            // the current cursor position in 3D world space 
             state.dragEndPoint = intersectionPoint;
+            // the current drag, difference between the current cursor position and 
+            // the hit point 
             const dragWorld = state.dragEndPoint.clone().sub(state.clickedOnPoint);
 
-
             let axes = [new THREE.Vector3(1, 0, 0),
-            new THREE.Vector3(0, 1, 0),
-            new THREE.Vector3(0, 0, 1)];
+                        new THREE.Vector3(0, 1, 0),
+                        new THREE.Vector3(0, 0, 1)];
             let names = ['x', 'y', 'z'];
             let inPlaneAxes = axes.filter((_, i) => names[i] !== state.normalAxis);
 
+            // want to compute the axis that's closest to the drag vector
             let best = null, bestDot = 0;
             for (const axis of inPlaneAxes) {
                 let d = dragWorld.dot(axis);
                 if (Math.abs(d) > Math.abs(bestDot)) { bestDot = d; best = axis; }
             }
+
             state.dragDir = best.clone().multiplyScalar(Math.sign(bestDot));
             let tempRotationAxis = state.worldNormal.clone().cross(state.dragDir);
+
+            // this switch cleans up dust which accumulates from repeated cross products
+            // the principal component won't return a clean axis vector, so we have to 
+            // map it ourselves
             let argmax = evil._principalComponent(tempRotationAxis);
             switch (argmax) {
                 case 'x':
@@ -134,16 +146,39 @@ function gestureMoveLogic(e, touched = false) {
                     break;
             }
 
+            // fix the sign of the rotation axis 
             state.rotateAroundAxis.multiplyScalar(Math.sign(tempRotationAxis[argmax]));
+
+            // selecting the layer to rotate by picking the cubelets which are within 
+            // a small threshold of the rotation axis 
             state.layerToRotate = cube.children.filter(cubelet =>
                 Math.abs(cubelet.rubikPosition.dot(state.rotateAroundAxis)
                     - state.clickedOnCubeletPosition.dot(state.rotateAroundAxis)) < 1e-6);
+
+            // have to attach the cubelets to a pivot parent object, whose only 
+            // purpose in life is to rotate cubelet layers 
             pivot = new THREE.Object3D();
             pivot.position.copy(evil.center);
             rubiks.scene.add(pivot);
             state.layerToRotate.forEach(cubelet => pivot.attach(cubelet));
+
+            // convert the drag vector to a scalar representing distance of drag
+            // the dot product takes the drag direction vector and applies it to the 
+            // current drag vector to get a scalar distance 
             state.dragDistance = currentDragWorld.dot(state.dragDir);
             stateMachine.update("dragging");
+        }
+    }
+    else if (stateMachine.dragging) {
+        if (intersectionPoint && result) {
+            state.dragDistance = currentDragWorld.dot(state.dragDir)
+            let angle = _dragAngle(state.dragDistance);
+            // rotate just the pivot
+            // can't mutate the rubikPosition attributes on the cubelets
+            // the drag rotation is just a move preview 
+            const q = new THREE.Quaternion().setFromAxisAngle(state.rotateAroundAxis, angle);
+            pivot.quaternion.copy(q);
+            pivot.updateMatrixWorld(true);
         }
     }
 }
@@ -165,6 +200,7 @@ function gestureUpLogic(e, touched = false) {
     const q = new THREE.Quaternion().setFromAxisAngle(state.rotateAroundAxis, angle);
     state.layerToRotate.forEach(c => c.rubikPosition.sub(evil.center).applyQuaternion(q).add(evil.center));
     let grid = [0, 1 / 3, 2 / 3];
+    // need to snap the cubelets back to their proper coordinates in the cube lattice 
     const snap = v => grid.reduce((b, g) =>
         Math.abs(v - g) < Math.abs(v - b) ? g : b);
     state.layerToRotate.forEach(cubelet => cubelet
